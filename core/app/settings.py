ed6c6a3d74c7b337c8f37c3df11c4f33050d1145
Sulatn Roziyev <jjo152065@gmail.com>
2023-03-09 20:06:54 +0600

    Добавил файлы проекта

--- core/app/settings.py
+++ core/app/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hm_57',
+        'NAME': 'hm',
         'USER': 'postgres',
         'PASSWORD': 'sula18',
         'HOST': 'localhost',
--- core/webapp/models/__init__.py
+++ core/webapp/models/__init__.py
@@ -1,3 +1,4 @@
+from .project import Project
 from .tasks import Tasks
 from .status import Status
 from .task_type import Type
