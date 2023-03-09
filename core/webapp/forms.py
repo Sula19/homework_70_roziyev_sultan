6e518d70fae461319f91b779c118fd97fd770246
Sulatn Roziyev <jjo152065@gmail.com>
2023-03-09 20:43:44 +0600

    Изменил форму, добавил форму проекта и поиска

--- core/webapp/forms.py
+++ core/webapp/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from django.forms import widgets
-from webapp.models import Tasks
-from webapp.validators import CustomSummaryValidator, CustomDescriptionValidator
+from webapp.models import Tasks, Project
+from webapp.validators import CustomTitleValidator, CustomDescriptionValidator
 
 
 class TasksForm(forms.ModelForm):
-    summary = forms.CharField(validators=(CustomSummaryValidator(),))
+    summary = forms.CharField(validators=(CustomTitleValidator(),))
     description = forms.CharField(widget=widgets.Textarea(), validators=(CustomDescriptionValidator(),))
 
     class Meta:
@@ -17,3 +17,23 @@ class TasksForm(forms.ModelForm):
             'status': 'Статус',
             'type': 'Тип',
         }
+
+
+class ProjectForm(forms.ModelForm):
+    start_date = forms.DateField(widget=widgets.DateInput(format='%d-%m-%Y', attrs={'type': 'date'}), label='Дата начала')
+    expiration_date = forms.DateField(widget=widgets.DateInput(format='%d-%m-%Y', attrs={'type': 'date'}), label='Дата окончания')
+    name = forms.CharField(validators=(CustomTitleValidator(),))
+    description = forms.CharField(widget=widgets.Textarea(), validators=(CustomDescriptionValidator(),))
+    class Meta:
+        model = Project
+        fields = ('start_date', 'expiration_date', 'name', 'description')
+        labels = {
+            'name': 'Название',
+            'description': 'Описание',
+            'start_date': 'Дата начала',
+            'expiration_date': 'Дата окончания'
+        }
+
+
+class SearchView(forms.Form):
+    search = forms.CharField(max_length=90, required=False, label='Найти')
